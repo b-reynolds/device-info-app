c0fbafaefdf379290f7c7ce3fd5467cd587ed7ca
Troy Rijkaard <troyrijkaard@hotmail.co.uk>
2020-10-02 20:36:05 +0100

    style: remove redundant braces from class

--- app/src/main/java/com/example/deviceinfo/dataitems/ManufacturerData.kt
+++ app/src/main/java/com/example/deviceinfo/dataitems/ManufacturerData.kt
@@ -5,7 +5,7 @@ import android.os.Build
 /**
  * Provides information on the Manufacturer of the current device.
  */
-class ManufacturerData() : DataItem {
+class ManufacturerData : DataItem {
     override val label: String get() = "Manufacturer"
     override val value: String get() = Build.MANUFACTURER
 }

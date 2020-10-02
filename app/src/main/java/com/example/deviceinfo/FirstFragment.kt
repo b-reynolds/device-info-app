20a491c1702dc18b32868fecb7323bf85d9c1aac
Owen <youngman.owen@gmail.com>
2020-10-02 23:42:16 +0100

    refactor: fix existing code to match ktlint

--- app/src/main/java/com/example/deviceinfo/FirstFragment.kt
+++ app/src/main/java/com/example/deviceinfo/FirstFragment.kt
@@ -14,8 +14,9 @@ import androidx.navigation.fragment.findNavController
 class FirstFragment : Fragment() {
 
     override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_first, container, false)

6100ca71f25f45b81bf4b3e66e5c3c8a877bf375
Owen <youngman.owen@gmail.com>
2020-10-03 12:03:32 +0100

    style: fix import ordering

--- app/src/main/java/com/example/deviceinfo/MainActivity.kt
+++ app/src/main/java/com/example/deviceinfo/MainActivity.kt
@@ -1,11 +1,11 @@
 package com.example.deviceinfo
 
 import android.os.Bundle
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
 

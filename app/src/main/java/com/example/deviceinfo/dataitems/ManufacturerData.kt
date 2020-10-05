088a5008ef06e28f1ab28ef58d1119b5e10c8c24
Chesire <Chesire@users.noreply.github.com>
2020-10-05 20:34:31 +0100

    feat: get manufacturer data (#9)
    * feat: add interface for data items to implement
    
    Add a basic interface for any data items to implement
    
    * feat: add class to pull manufacturer
    
    Add infoitem to get the Manufacturer
    
    * refactor: rename from InfoItem to DataItem
    
    Change the naming of the InfoItem to be a DataItem as it makes more
    sense when reading it as a bit of data, and also more fits the
    DeviceInfo
    
    * style: remove redundant braces from class

---
+++ app/src/main/java/com/example/deviceinfo/dataitems/ManufacturerData.kt
@@ -0,0 +1,11 @@
+package com.example.deviceinfo.dataitems
+
+import android.os.Build
+
+/**
+ * Provides information on the Manufacturer of the current device.
+ */
+class ManufacturerData : DataItem {
+    override val label: String get() = "Manufacturer"
+    override val value: String get() = Build.MANUFACTURER
+}
